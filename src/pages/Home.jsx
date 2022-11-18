6e315393ade94d5ba86b59fe049e416b70b6405e
lmleopold <louismarie.leopold@gmail.com>
2022-11-18 11:09:59 +0100

    Config envt du travail REACT + creation des pages + ajouts des fichiers assets

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import logo from "../logo.svg";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Kaza</h1>
+      <div>{logo}</div>
+    </div>
+  );
+}
+
+export default Home;
