d7356c13c5a18e9799dd542499ab0f3162925ebf
lmleopold <louismarie.leopold@gmail.com>
2022-11-29 11:09:13 +0100

    simplification code

--- src/components/carroussel/Carroussel.jsx
+++ src/components/carroussel/Carroussel.jsx
@@ -3,72 +3,52 @@ import ArrowLeft from "../../assets/button/ArrowLeft";
 import styles from "./carrousel.module.scss";
 import { useState } from "react";
 
-/**
- * Extrait le numéro de l'image entrée en argument
- * @param {String} pictureURL
- */
-function numberOfPicture(pictureURL) {
-  return pictureURL.slice(
-    pictureURL.lastIndexOf("-") + 1,
-    pictureURL.indexOf(".jpg")
-  );
-}
-
-function scrollRight(indexDisplayed, numberOfPictures) {
-  console.log(numberOfPictures);
-  if (indexDisplayed === numberOfPictures) {
-    indexDisplayed = 0;
-  } else {
-    indexDisplayed++;
-  }
-  console.log({ indexDisplayed }, numberOfPictures);
-  return indexDisplayed;
-}
-
-function scrollLeft(indexDisplayed, numberOfPictures) {
-  if (indexDisplayed === 0) {
-    indexDisplayed = numberOfPictures;
-  } else {
-    indexDisplayed--;
-  }
-  return indexDisplayed;
-}
-
 function Carroussel({ cover, pictures }) {
-  let indexCover = pictures.findIndex(
-    (picture) => numberOfPicture(picture) === numberOfPicture(cover)
-  );
-  const [indexDisplayed, unpdateIndex] = useState(indexCover);
-  let activePicture = pictures[indexDisplayed];
+  let indexCover = pictures.findIndex((picture) => cover === picture);
+  const [indexTab, updateIndex] = useState(indexCover);
+
+  // function numberOfPicture(pictureURL) {
+  //   return pictureURL.slice(
+  //     pictureURL.lastIndexOf("-") + 1,
+  //     pictureURL.indexOf(".jpg")
+  //   );
+  // }
+
+  function scrollRight() {
+    if (indexTab === pictures.length - 1) {
+      updateIndex(0);
+    } else {
+      updateIndex((index) => index + 1);
+    }
+  }
+
+  function scrollLeft() {
+    if (indexTab === 0) {
+      updateIndex(pictures.length - 1);
+    } else {
+      updateIndex((index) => index - 1);
+    }
+  }
+
   return pictures.length === (0 || 1) ? (
     <header className={styles.banner}>
       <div className={styles.masque}></div>
-      <img src={activePicture} className={styles.img} alt=""></img>
+      <img src={pictures[indexTab]} className={styles.img} alt=""></img>
     </header>
   ) : (
     <header className={styles.banner}>
       <div className={styles.masque}></div>
-      <img src={activePicture} className={styles.img} alt=""></img>
+      <img src={pictures[indexTab]} className={styles.img} alt=""></img>
       <div className={styles.arrows}>
-        <button
-          className={styles.arrowButton}
-          onClick={() =>
-            unpdateIndex(scrollLeft(indexDisplayed, pictures.length - 1))
-          }
-        >
+        <button className={styles.arrowButton} onClick={scrollLeft}>
           <ArrowLeft />
         </button>
-        <button
-          className={styles.arrowButton}
-          onClick={() =>
-            unpdateIndex(scrollRight(indexDisplayed, pictures.length - 1))
-          }
-        >
+        <button className={styles.arrowButton} onClick={scrollRight}>
           <ArrowRight />
         </button>
       </div>
       <p className={styles.indexPicture}>
-        {indexDisplayed + 1}/{pictures.length}
+        {indexTab + 1}/{pictures.length}
       </p>
     </header>
   );
