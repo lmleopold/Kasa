1db4eb84dced0b6c2b3e8544126f640bca616539
lmleopold <louismarie.leopold@gmail.com>
2022-12-08 02:43:00 +0100

    page NotFound 100% responsive

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -3,8 +3,10 @@ import styles from "./notFound.module.scss";
 
 function NotFound() {
   return (
-    <main className={styles.container}>
-      <h1 className={styles.title}>404</h1>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>404</h1>
+      </div>
       <p className={styles.subtitle}>
         Oups! La page que vous demandez n'existe pas.
       </p>
